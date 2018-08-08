6f7d6ea2e9e12678c31e2c64de2c1c2a94bb6f3e
Benyapa <ye.amity@gmail.com>
2018-08-09 00:53:14 +0900

    first commit

---
+++ Assets/GameManagerClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerClass : MonoBehaviour {
+
+	bool gameHasEnd = false;
+	bool winGame = false;
+
+	public void EndGame() {
+		if (gameHasEnd == false) {
+			gameHasEnd = true;
+			Debug.Log ("Game Over");
+			SceneManager.LoadScene("GameOver");
+		}
+	}
+
+	public void WinTheGame() {
+		if (winGame == false) {
+			winGame = true;
+			SceneManager.LoadScene ("Win");
+		}
+	}
+
+}
---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingMenu : MonoBehaviour {
+
+	public void SetVolume (float volume) {
+		Debug.Log (volume);
+	}
+}
